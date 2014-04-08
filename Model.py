b8e5629b6b78d804f936ba61d3e0cfac0a6dcdd4
davesullivan41 <davesullivan41@gmail.com>
2014-04-07 21:41:21 -0500

    Added Model.py

---
+++ Model.py
@@ -0,0 +1,205 @@
+from __future__ import print_function, division
+import numpy as np
+import matplotlib.pyplot as plt
+import Euler_Methods as em
+import os.path
+
+def orbitPlot(center,moons,r,figure,names):
+  plt.plot(0.,0.,'o')
+  for index in moons:
+    plt.plot(r[:,index,0]-r[:,center,0],r[:,index,1]-r[:,center,1])
+
+  plt.title(names[center]+' and satellites')
+
+def orbitPlotSat(center,outer):
+  plt.plot(0.,0.,'o')
+  plt.plot(outer[:,0]-center[:,0],outer[:,1]-center[:,1])
+ 
+
+# orbit - Program to compute the orbit of a comet.
+#clear all;  help orbit;  % Clear memory and print header
+
+# Set initial position and velocity of the comet.
+# r0 = float(raw_input("Enter initial radial distance (AU): "))
+# v0 = float(raw_input("Enter initial tangential velocity (AU/yr): "))
+# matrices are ordered in radial distance from the sun (i.e. the sun first ,jupiter last)
+# TODO: Read these from file that contains values in kg/m/s units, and translate
+# r = np.array([[1.0022,0.],[0.,0.],[1.,0.],[1.0024,0.],[1.52,0.],[5.2,0.]])
+# v = np.array([[0.,6.493185],[0.,0.],[0.,6.283185],[0.,6.0],[0.,5.09634],[0.,2.75536]])
+
+
+# decrease units by ~9 orders of magnitude
+au = 1.496e11
+earthV = 2.978e4
+solarMass = 1.989e30
+velConversion = 2.108e-4
+names = ['Sun','Venus','Earth','Moon','Mars','Jupiter']
+# r = np.array([[au+5e7,0.],[0.,0.],[au,0.],[au+3.633e8,0.],[2.289e11,0.],[7.785e11,0.]])
+# v = np.array([[0.,earthV + 6e3],[0.,0.],[0.,earthV],[0.,earthV + 1.022e3],[0.,2.408e4],[0.,1.307e4]])
+r = np.array([[0.,0.],[1.0894e11,0.],[au,0.],[au+3.633e8,0.],[2.289e11,0.],[7.785e11,0.]])
+v = np.array([[0.,0.],[0.,3.5e4],[0.,earthV],[0.,earthV + 1.022e3],[0.,2.408e4],[0.,1.307e4]])
+# convert from m,s to au,year
+r = r/au
+v = v * velConversion
+# 6.493185
+# manually create the sun
+state = np.array([[ r[0,0], r[0,1], v[0,0], v[0,1] ]])
+pCount = 1
+totalPlanets = len(r)
+while pCount < totalPlanets:
+  state = np.append(state,np.array([[r[pCount,0], r[pCount,1], v[pCount,0],v[pCount,1]]]),0)
+  pCount += 1
+
+#Set physical parameters (mass, G*M)
+GM = 4*np.pi**2      # Grav. const. * Mass of Sun (au^3/yr^2)
+# GM = 6.674e-11         # Grav. const. 
+# mass in fraction of sun mass
+# mass = np.array([3.7e-14,1.,3.e-6,3.7e-8,3.3e-7,9.5e-4])
+mass = np.array([1.989e30,4.8676e24,5.972e24,7.348e22,6.418e23,1.899e27])
+# conver to solar masses
+mass = mass/solarMass
+#mass = np.array([(1./3.)*1.e6,1.,316.+(2./3.)])
+adaptErr = 10.# Error parameter used by adaptive Runge-Kutta
+time = 0.0
+
+#%* Loop over desired number of steps using specified
+#%  numerical method.
+tau =  float(raw_input("Enter time step (yr): "))
+NumericalMethod = 4
+maxSteps = int(1./tau)
+filename = 'dat/solarSystem_'+str(maxSteps)+'_'+str(tau)+'.txt'
+if(os.path.isfile(filename)):
+  print('Loading file from data store')
+  planetsRaw = np.loadtxt(filename)
+  totalLength = len(planetsRaw)
+  planetsRaw = planetsRaw.tolist()
+  numPlanets = 6
+  pLength = int(totalLength/numPlanets)
+
+  # rComp = np.array([np.array([planetsRaw[0],planetsRaw[pLength],planetsRaw[2*pLength],
+  #   planetsRaw[3*pLength],planetsRaw[4*pLength],planetsRaw[5*pLength]])])
+  rComp = [[planetsRaw[0],planetsRaw[pLength],planetsRaw[2*pLength],
+    planetsRaw[3*pLength],planetsRaw[4*pLength],planetsRaw[5*pLength]]]
+
+  # rComp = np.array([np.array()])
+  for i in range(1,int(pLength)):
+    rComp.append([planetsRaw[0+i],planetsRaw[pLength+i],planetsRaw[2*pLength+i],
+      planetsRaw[3*pLength+i],planetsRaw[4*pLength+i],planetsRaw[5*pLength+i]])
+  rComp = np.array(rComp)
+  # print(rComp[2])
+  tplot = np.arange(pLength)*tau+time
+else:
+  orbits = 0
+  maxOrbits = 2
+  ## TODO: Make loop stop at predetermined time, not after running out of moves
+  print('Loading...')
+  for istep in range(0,maxSteps):
+
+    # Progress bar
+    if(istep%(maxSteps/10) == 0):
+      print('|',end="")
+    elif(istep%(maxSteps/50) == 0):
+      print('-',end="")
+    #%* Record position and energy for plotting.
+    # Initially set the arrays for the first step
+    if istep == 0:
+      # record both r components
+      rComp = np.array(np.array([r]))
+      tplot = time
+    else:
+      rComp = np.append(rComp,np.array([r]),axis=0)
+      tplot = np.append(tplot,time)
+
+    # orbit count break
+    # if(istep > 2 and thplot[-2,4] < 0 and thplot[-1,4] >= 0):
+      # break  
+
+    state = em.EulerCromer(state,mass,time,tau,em.gravmatrk)
+    r = np.copy(state[:,0:2])
+    v = np.copy(state[:,2:4])
+    # print(v)
+    # time = time + tau
+  print('|')
+  #myPub = PdfPages('ExB.pdf')
+
+  with file(filename,'w') as outfile:
+    for planet in range(0,totalPlanets):
+      outfile.write('# New planet\n')
+      np.savetxt(outfile,rComp[:,planet])
+print('Planet model built')
+
+
+
+####################################################################
+## Now that the solar system has been built, place the spacecraft ##
+## into the system and have it accelerate away from starting point##
+####################################################################
+## Probably need to decrease time step to ~ minutes or seconds
+# r = np.array([au+4.2164e7,0.])
+# v = np.array([0.,earthV-3.075e3])
+r = [(au+4.2164e7)/au,0.]
+v = [0.,(earthV-3.075e3)*velConversion]
+satMass = 1.
+
+## TEST ###
+# r = np.array([1.28939e11,0.])
+# v = np.array([0,3.5e4])
+# satMass = 4.8676e24
+###########
+
+# r = r/au
+# v = v * velConversion
+satMass = satMass/solarMass
+state = np.array([r[0],r[1],v[0],v[1]])
+step = 0
+
+filename = 'dat/satellite_'+str(maxSteps)+'_'+str(tau)+'.txt'
+if(os.path.isfile(filename)):
+  rSComp = np.loadtxt(filename)
+  print('Satellite path loaded from data store')
+else:
+  # rSComp = np.array(np.array([r]))
+  rSComp = [r]
+  # points to interp over
+  planets = np.copy(rComp[step:step+3])
+  [state,satMass] = em.EulerCromerSat(state,planets,satMass,mass,tplot[1],tau,em.gravSat)
+  r = np.copy(state[0:2])
+  v = np.copy(state[2:4])
+  step += 1
+  # print(rComp[step-1:step+2])
+  for time in tplot[2:-1]:
+    # rSComp = np.append(rSComp,np.array([r]),axis=0)
+    rSComp.append(r.tolist())
+    # points to interp over
+    planets = np.copy(rComp[step-1:step+2])
+    [state,satMass] = em.EulerCromerSat(state,planets,satMass,mass,tplot[time],tau,em.gravSat)
+    r = np.copy(state[0:2])
+    v = np.copy(state[2:4])
+    step += 1
+  #### TODO: Build method to load data to file, then build another function that turns it into a video
+  rSComp = np.array(rSComp)
+  np.savetxt(filename,rSComp)
+  print('Satellite path calculated')
+
+# Plot the whole system
+fig = plt.figure(1); plt.clf()  #Clear figure 1 window and bring forward
+orbitPlot(0,[1,2,3,4,5],rComp,fig,names)
+plt.xlabel('Distance (AU)')
+plt.grid('on')
+
+# Satellite trajectory from Earth frame
+fig = plt.figure(2)
+# orbitPlot(2,[3],rComp,fig,names)
+plt.plot(rSComp[:,0] - rComp[1:-1,2,0],rSComp[:,1]-rComp[1:-1,2,1])
+
+# Satellite trajectory from Solar frame
+fig = plt.figure(3)
+plt.plot(rComp[:,2,0],rComp[:,2,1],rSComp[:,0],rSComp[:,1])
+
+fig = plt.figure(4)
+satOrbitR = np.sqrt((rSComp[:,0]-rComp[1:-1,2,0])**2 + (rSComp[:,1]-rComp[1:-1,2,1])**2)
+plt.plot(satOrbitR)
+plt.title('Satellite distance from Earth')
+
+
+plt.show()
